d5aad481a2e114511b38431735011e342040fbb3
tlaranje <tlaranje@student.42.porto.com>
2025-11-18 13:24:42 +0000

    :)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tlaranje <tlaranje@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:46:55 by tlaranje          #+#    #+#             */
-/*   Updated: 2025/11/18 11:43:02 by tlaranje         ###   ########.fr       */
+/*   Updated: 2025/11/18 13:24:14 by tlaranje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-void	ft_del(void *content);
+void	del(void *content);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *newc);
